d8cf826474dff5b8b29b28f81904accf231b5203
Mark <mark.davidovich@gmail.com>
2026-01-23 17:19:05 +0200

    create controller with unfinished run function

--- controller.js
+++ controller.js
@@ -1,1 +1,30 @@
-import { AutoCompleteTrie } from "./model";
+const { printHelp, actionsMenu, printMessage, printStartMessage, printExitMessage } = module.require("/view");
+const AutoCompleteTrie = module.require("/model");
+
+const trie = new AutoCompleteTrie(" ");
+
+const run = () => {
+  printStartMessage();
+  while (true) {
+    const userInput = actionsMenu();
+
+    if (!input) continue;
+
+    if (userInput.action === "exit") {
+      printExitMessage();
+      break;
+    }
+
+    switch (userInput.action) {
+      case "add":
+        //
+        break;
+      case "find":
+        //
+        break;
+      case "complete":
+        //
+        break;
+    }
+  }
+};
