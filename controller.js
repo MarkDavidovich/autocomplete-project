247aebddeef94ea940b5f9843b094f401751a374
Mark <mark.davidovich@gmail.com>
2026-01-23 18:15:30 +0200

    implemented controller actions

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
-const { printHelp, actionsMenu, printMessage, printStartMessage, printExitMessage } = module.require("/view");
-const AutoCompleteTrie = module.require("/model");
+const { printHelp, actionsMenu, printMessage, printStartMessage, printExitMessage } = module.require("./view");
+const AutoCompleteTrie = module.require("./model");
 
 const trie = new AutoCompleteTrie(" ");
 
@@ -8,7 +8,7 @@ const run = () => {
   while (true) {
     const userInput = actionsMenu();
 
-    if (!input) continue;
+    if (!userInput) continue;
 
     if (userInput.action === "exit") {
       printExitMessage();
@@ -17,14 +17,31 @@ const run = () => {
 
     switch (userInput.action) {
       case "add":
-        //
+        const newWord = trie.addWord(userInput.word);
+        if (!newWord) {
+          printMessage(false, ` "${userInput.word}" already exists in dictionary`);
+          break;
+        }
+        printMessage(true, ` Added "${userInput.word}" to dictonary`);
         break;
       case "find":
-        //
+        const hasFoundWord = trie.findWord(userInput.word);
+        if (!hasFoundWord) {
+          printMessage(false, ` "${userInput.word}" not found in dictionary`);
+          break;
+        }
+        printMessage(true, ` "${userInput.word}" exists in dictionary`);
         break;
       case "complete":
-        //
+        const wordList = trie.predictWords(userInput.word);
+        if (wordList === null || wordList.length === 0) {
+          printMessage(false, ` No suggestions for "${userInput.word}"`);
+          break;
+        }
+        printMessage(true, ` Suggestion for "${userInput.word}": ${wordList.join(", ")} `);
         break;
     }
   }
 };
+
+run();
